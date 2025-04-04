8b2017433247fe7cc35a0eb671b78fafac895ca5
MaximoLagos <mlagosmarmol1@mail.austral.edu.ar>
2025-04-03 23:02:03 -0300

    Add files via upload

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"\t{first_name.lower()} {last_name.lower()}")
+
+
+ada()
